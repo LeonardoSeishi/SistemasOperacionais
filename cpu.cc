ac77a7c59ee78e9bc5fd11a7a311babca2a4416b
LeonardoSeishi <leonardosyamazaki@gmail.com>
2023-03-17 09:33:18 -0300

    tentativa 1 de fato

--- cpu.cc
+++ cpu.cc
@@ -5,24 +5,26 @@ __BEGIN_API
 
 void CPU::Context::save()
 {
-    getcontext();
+    getcontext(&this->_context);
 }
 
 void CPU::Context::load()
 {
-    setcontext();
+    setcontext(&this->_context);
 }
 
 CPU::Context::~Context()
 {
-    if (_stack) {       //Se existe uma pilha
-        delete (_stack);//deletar a pilha
-    }
+    try { 
+        delete (_stack);}
+    catch(...) { 
+        std::cout << "Não existe uma pilha";}
+        
 }
 
 int CPU::switch_context(Context *from, Context *to)
 {
-     swapcontext();
+     swapcontext(&from->_context, &to->_context);
 }
 
 __END_API
