2775a199e386e5539611c1faa1012711c4b060a6
LeonardoSeishi <leonardosyamazaki@gmail.com>
2023-03-30 16:43:28 -0300

    System.cc

---
+++ src/system.cc
@@ -0,0 +1,16 @@
+#ifndef system_h
+#define system_h
+
+#include <stdio.h>
+#include "../include/traits.h"
+
+__BEGIN_API
+
+class System {
+
+    public:
+        static void init();
+
+};
+
+__END_API
