87f863c97f65832a8966f1405af47542b61df9f3
Tainá da Cruz <tainadacruz1234@gmail.com>
2023-03-30 09:43:57 -0300

    feat: adicionando template etc

--- src/thread.cc
+++ src/thread.cc
@@ -2,9 +2,16 @@
 
 __BEGIN_API
 
+Thread * Thread::_running = nullptr;
+
 static int switch_context(Thread *prev, Thread *next)
 {
     return CPU::switch_context(prev->get_self_context(), next->get_self_context());
 }
+
+int Thread::id() {
+    return _id;
+}
+
 
 __END_API
