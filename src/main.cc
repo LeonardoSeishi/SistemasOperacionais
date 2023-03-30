d94916d160692945c66ab68be7a597a811f4cf1c
LeonardoSeishi <leonardosyamazaki@gmail.com>
2023-03-30 13:16:17 -0300

    system na main

--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,14 @@
 #include "cpu.h"
 #include <iostream>
 #include "./main_class.h"
+#include "./system.h"
 
 __USING_API 
 
 
 int main(void)
 {
+    System::init();
     Main::run();
     
     return 0;
